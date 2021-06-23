0e7b515e36f471066f73c4f75820103def099b1c
Jasur <developer7102@gmail.com>
2021-06-23 14:45:53 +0200

    add css chane

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
-var elsOpenModalButton = document.querySelectorAll('.links__right');
+var elsOpenModalButton = document.querySelector('.links__right');
 var elCloseModalButton = document.querySelector('.form__btn');
 var elModal = document.querySelector('.js-model');
 
-elOpenModalButton.forEach(btn => {
-  btn.addEventListener('click', function (e) {
-    e.preventDefault();
-    elModal.classList.add('model-open');
-  });
+elsOpenModalButton.addEventListener('click', function(){
+  elModal.classList.add('open-modal')
 });
 
-elCloseModalButton.addEventListener('click', function () {
-  elModal.classList.remove('model-open');
-});
-
+elCloseModalButton.addEventListener('click', function(){
+elModal.classList.remove('open-modal')
+})
